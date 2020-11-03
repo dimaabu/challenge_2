6bdf3346217ffddf76992573e54afb168516df2b
dimaabu <deemaabuzahra@outlook.com>
2020-11-03 19:19:48 +0200

    g

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const fs = require('fs')
+const port =4321
+const app = express();
+app.listen(4321, () => console.log('listening at 3000'));
+app.use (express.static('client'));
+
+
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+  })
+
+
 
 
